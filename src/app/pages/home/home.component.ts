bc1605f5494cf67eb1626cd2297bf9903b45c379
isaias r. lopez <155261549+qwik-zgheib@users.noreply.github.com>
2024-09-04 17:44:49 -0500

    feat: add google fonts [montserrat, montserrat alternates, poppins, victor mono]

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHome, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FontAwesomeModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  faHome = faHome;
+  faChevronRight = faChevronRight;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ['Montserrat', 'sans-serif'],
+        'mont-alt': ['Montserrat Alternates', 'sans-serif'],
+        popp: ['Poppins', 'sans-serif'],
+        'victor-mono': ['Victor Mono', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     require('@catppuccin/tailwindcss')({
