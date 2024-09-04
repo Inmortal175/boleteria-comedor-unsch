7f5597393fcd26df02fa2677359d45c51da5ab5c
isaias r. lopez <155261549+qwik-zgheib@users.noreply.github.com>
2024-09-04 17:01:19 -0500

    feat: fontawesone + basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+// please modularize: auth -> sign-in & sign-up
+
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+];
