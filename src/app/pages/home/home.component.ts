c5bfe5cf863da94d292725df7e06ac40678392ab
isaias r. lopez <155261549+qwik-zgheib@users.noreply.github.com>
2024-09-04 22:15:43 -0500

    feat: i need batadase model for test

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,4 +12,77 @@ import { faHome, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 export class HomeComponent {
   faHome = faHome;
   faChevronRight = faChevronRight;
+  itemsLeft = [
+    { id: 1, text: 'Ticket' },
+    { id: 2, text: 'Pase Semanal' },
+    { id: 3, text: '[beca-comedor]' },
+  ];
+  dataMenu = [
+    {
+      field: 'desayuno',
+      details: [
+        {
+          name: 'Bebible',
+          value: ['cocoa', 'leche'],
+        },
+        {
+          name: 'acompañamiento',
+          value: ['pan con mermelada', 'aceituna'],
+        },
+        {
+          name: 'aditivo',
+          value: 'pan',
+        },
+        {
+          name: 'fruta / postre',
+          value: 'papaya',
+        },
+      ],
+      iamge: 'example_url_image.com',
+    },
+    {
+      field: 'almuerzo',
+      details: [
+        {
+          name: 'sopa de entrada',
+          value: 'sopa de trigo',
+        },
+        {
+          name: 'segundo',
+          value: ['estofado de pescado', 'arroz'],
+        },
+        {
+          name: 'ensalada',
+          value: ['lechuga', 'pepino', 'tomate'],
+        },
+        {
+          name: 'fruta / postre',
+          value: 'durazno',
+        },
+        {
+          name: 'refresco',
+          value: 'capuli',
+        },
+      ],
+      iamge: 'example_url_image.com',
+    },
+    {
+      field: 'cena',
+      details: [
+        {
+          name: 'palto principal',
+          value: ['coliflor con pollo', 'arroz'],
+        },
+        {
+          name: 'postre',
+          value: 'tiramisu',
+        },
+        {
+          name: 'bebida',
+          value: 'cocoa',
+        },
+      ],
+      iamge: 'example_url_image.com',
+    },
+  ];
 }
