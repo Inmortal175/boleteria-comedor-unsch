a27cdd0880dcd05f3948e7c4a0bfb7331493f003
lc <jhoncitof1ea10@gmail.com>
2024-09-21 12:19:41 -0500

    chore: modify configuration at eslint+prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'boleteria-comedor-unsch ';
+  title = 'boleteria-comedor-unsch';
 }
